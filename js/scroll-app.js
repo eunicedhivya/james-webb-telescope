a32ac1fce6edad443a819eceac4416494e91810e
Eunice Dhivya <divi.seed@gmail.com>
2021-12-19 10:51:54 +0530

    modify scroll animation based on washington example

--- js/scroll-app.js
+++ js/scroll-app.js
@@ -28,21 +28,37 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var timelineOrigin = new TimelineMax();
     timelineOrigin
-        .from('#origins-para1', 4, {
-            x: 50,
-            y: 0,
+        .from('#origins-image', 4, {
             opacity: 0,
             ease: Power0.easeOut
         })
-        .from('#origins-image', 4, {
-            x: -50,
-            y: 0,
+        .from('#origins-para1', 4, {
+            top: "100vh",
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('#origins-para1', 20, {
+            top: "80vh",
+            opacity: 1,
+            ease: Power0.easeOut
+        })
+        .to('#origins-para1', 20, {
+            top: "0vh",
             opacity: 0,
             ease: Power0.easeOut
         })
         .from('#origins-para2', 4, {
-            x: 0,
-            y: 150,
+            top: "100vh",
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('#origins-para2', 20, {
+            top: "80vh",
+            opacity: 1,
+            ease: Power0.easeOut
+        })
+        .to('#origins-para2', 20, {
+            top: "0vh",
             opacity: 0,
             ease: Power0.easeOut
         })
@@ -67,23 +83,46 @@ document.addEventListener('DOMContentLoaded', function () {
             opacity: 0,
             ease: Power0.easeOut
         })
+        .from('.tr-txt-4', 4, {
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('.tr-txt-3', 4, {
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('.tr-txt-2', 4, {
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('.tr-splitarrow', 4, {
+            opacity: 0,
+            x: -10,
+            ease: Power0.easeOut
+        })
+        .from('.tr-txt-1', 4, {
+            opacity: 0,
+            ease: Power0.easeOut
+        })
         .from('.tr-bigbang', 4, {
             opacity: 0,
             ease: Power0.easeOut
         })
-        .from('.tr-hubble-icon', 4, {
+        .from('.hubble-grp', 4, {
             opacity: 0,
+            y: -50,
             ease: Power0.easeOut
         })
-        .from('.tr-hubble-icon', 4, {
+        .from('.webb-grp', 4, {
             opacity: 0,
+            y: -50,
             ease: Power0.easeOut
         })
 
-    var scene2 = new ScrollMagic.Scene({
+    var scene3 = new ScrollMagic.Scene({
         triggerElement: '.the-range',
         duration: '100%',
         triggerHook: 0,
-    }).setTween(timelineTheRange).setPin(".the-range").addTo(controller)
+    }).setTween(timelineTheRange).setPin(".the-range").setClassToggle(".the-range", "active").addTo(controller)
 
 });
