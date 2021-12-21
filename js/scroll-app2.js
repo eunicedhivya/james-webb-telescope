312485debd03b2f96194fd28f8ffc8d988519d5d
Eunice Dhivya <divi.seed@gmail.com>
2021-12-21 15:57:51 +0530

    scroll animation

--- js/scroll-app2.js
+++ js/scroll-app2.js
@@ -1,16 +1,3 @@
-document.getElementById('enable3dwindow').addEventListener("click", function () {
-    document.getElementById('cover-3d').style.display = "none";
-    document.getElementsByTagName('body')[0].style.overflow = "hidden";
-    // document.getElementById('model').style.transform = "scale(1.5)";
-    // alert("test");
-})
-document.getElementById('disable3dwindow').addEventListener("click", function () {
-    document.getElementById('cover-3d').style.display = "block";
-    document.getElementsByTagName('body')[0].style = "null";
-    // document.getElementById('model').style.transform = "scale(1.5)";
-    // alert("test");
-})
-
 var images = [
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-001.jpg",
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-002.jpg",
@@ -130,191 +117,8 @@ var images = [
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-114.jpg",
 ];
 
-
-var video = document.getElementsByTagName('video')[0];
-var videoDuration;
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var controller = new ScrollMagic.Controller();
-
-    var timelineIntro = new TimelineMax();
-
-    timelineIntro
-        .from('.main-desc', 2, {
-            x: 0,
-            y: -50,
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-
-    var scene = new ScrollMagic.Scene({
-        triggerElement: '#intro',
-        duration: '100%',
-        triggerHook: 0,
-        offset: '0'
-    }).setTween(timelineIntro).setPin("#intro").addTo(controller)
-
-
-    var timelineOrigin = new TimelineMax();
-    timelineOrigin
-        .from('#origins-image', 500, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .to('#origins-image', 500, {
-            scale: 2,
-            ease: Power0.easeOut
-        })
-        .from('#origins-para1', 1000, {
-            y: 20,
-            top: "100vh",
-            ease: Power0.easeOut
-        })
-        .to('#origins-para1', 1000, {
-            y: -20,
-            top: "-100vh",
-            ease: Power0.easeOut
-        })
-        .from('#origins-para2', 1000, {
-            y: 20,
-            top: "100vh",
-            ease: Power0.easeOut
-        })
-        .to('#origins-para2', 1000, {
-            y: -20,
-            top: "-100vh",
-            ease: Power0.easeOut
-        })
-
-    var scene2 = new ScrollMagic.Scene({
-        triggerElement: '#origins',
-        duration: '200%',
-        triggerHook: 0,
-    }).setTween(timelineOrigin).setPin("#origins").addTo(controller)
-
-    var timelineTheRange = new TimelineMax();
-    timelineTheRange
-        .from('.tr-timeline', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-pointedLight', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-gridLine', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-txt-4', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-txt-3', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-txt-2', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-splitarrow', 5000, {
-            opacity: 0,
-            x: -10,
-            ease: Power0.easeOut
-        })
-        .from('.tr-txt-1', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.tr-bigbang', 5000, {
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('.hubble-grp', 5000, {
-            opacity: 0,
-            y: -50,
-            ease: Power0.easeOut
-        })
-        .from('.webb-grp', 5000, {
-            opacity: 0,
-            y: -50,
-            ease: Power0.easeOut
-        })
-        .from('#range-para1', 5000, {
-            delay: 0,
-            top: "120vh",
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .to('#range-para1', 10000, {
-            top: "80vh",
-            ease: Power0.easeOut
-        })
-        .to('#range-para1', 10000, {
-            top: "40vh",
-            ease: Power0.easeOut
-        })
-        .to('#range-para1', 10000, {
-            top: "-120vh",
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .from('#range-para2', 10000, {
-            delay: 0,
-            top: "120vh",
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-        .to('#range-para2', 10000, {
-            top: "80vh",
-            ease: Power0.easeOut
-        })
-        .to('#range-para2', 10000, {
-            top: "40vh",
-            ease: Power0.easeOut
-        })
-        .to('#range-para2', 10000, {
-            top: "-120vh",
-            opacity: 0,
-            ease: Power0.easeOut
-        })
-
-    var scene3 = new ScrollMagic.Scene({
-        triggerElement: '#telescope-range',
-        duration: '500%',
-        triggerHook: 0,
-    }).setTween(timelineTheRange).setPin("#telescope-range").setClassToggle("#telescope-range", ".active").addTo(controller)
-
-
-    var timelineTheLegrange = new TimelineMax();
-    timelineTheLegrange
-        .from('#lagrange-para1', 10000, {
-            delay: 1,
-            top: "120vh",
-            ease: Power0.easeOut
-        })
-        .to('#lagrange-para1', 10000, {
-            top: "80vh",
-            ease: Power0.easeOut
-        })
-        .to('#lagrange-para1', 10000, {
-            top: "20vh",
-            ease: Power0.easeOut
-        })
-        .to('#lagrange-para1', 10000, {
-            top: "-120vh",
-            ease: Power0.easeOut
-        })
-
-    var scene4 = new ScrollMagic.Scene({
-        triggerElement: '#the-lagrange',
-        duration: '200%',
-        triggerHook: 0,
-    }).setTween(timelineTheLegrange).setPin("#the-lagrange").setClassToggle("#the-lagrange", "active").addTo(controller)
-
-
 
     var obj = { curImg: 0 };
 
@@ -332,21 +136,11 @@ document.addEventListener('DOMContentLoaded', function () {
             ease: Linear.easeNone,			// show every image the same ammount of time
             onUpdate: function () {
                 $("#myimg").attr("src", reverseImg[obj.curImg]); // set the image source
+                console.log(obj.curImg)
             }
         }
-    );
-    // var tweenImg = TweenMax.to(obj, 0.5,
-    //     {
-    //         curImg: images.length - 1,	// animate propery curImg to number of images
-    //         roundProps: "curImg",				// only integers so it can be used as an array index
-    //         repeat: 3,									// repeat 3 times
-    //         immediateRender: true,			// load first image automatically
-    //         ease: Linear.easeNone,			// show every image the same ammount of time
-    //         onUpdate: function () {
-    //             $("#myimg").attr("src", images[obj.curImg]); // set the image source
-    //         }
-    //     }
-    // );
+    )
+
 
     var scene5 = new ScrollMagic.Scene({ triggerElement: "#telescope", duration: 5000, triggerHook: 0 })
         .setTween(tweenImg)
@@ -354,33 +148,10 @@ document.addEventListener('DOMContentLoaded', function () {
         .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
 
-    console.log(scene5)
+    scene5.on("change", function (event) {
+        console.log("Test");
+    });
 
 
-    // // Grab the target video duration
-    // videoDuration = video.duration;
-    // console.log(video.duration * 1000);
-
-    // // Tween the video from beginning to end
-    // var videoTween = TweenMax.to(video, 1, {
-    //     currentTime: videoDuration,
-    //     ease: Quad.easeOut,
-    //     overwrite: true,
-    //     pause: true
-    // });
-
-    // // Build ScrollMagic Scene
-    // var scene5 = new ScrollMagic.Scene({ triggerElement: "#telescope", duration: videoDuration * 1000, offset: 0 })
-    //     .setTween(videoTween) // Add tween to the scene
-    //     .addIndicators({ name: "video play" }) // Add indicators to the scrollbar							
-    //     .setPin("#telescope") // Add indicators to the scrollbar	
-    //     .setClassToggle("#telescope", "active")
-    //     .addTo(controller); // Add scene to the controller
-
-    // var scene6 = new ScrollMagic.Scene({
-    //     triggerElement: '#jwst-deployment',
-    //     duration: '100%',
-    //     triggerHook: 0,
-    // }).setTween().setPin("#jwst-deployment").setClassToggle("#jwst-deployment", "active").addTo(controller)
 
 });
