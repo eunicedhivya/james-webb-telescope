256b7dcbdd4de3f662bb59a37a307838758e0d29
Eunice Dhivya <divi.seed@gmail.com>
2021-12-22 00:09:33 +0530

    add text frames based on image seq  scroll pos

--- js/scroll-app.js
+++ js/scroll-app.js
@@ -128,6 +128,120 @@ var images = [
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-112.jpg",
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-113.jpg",
     "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-114.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-114.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-113.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-112.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-111.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-110.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-109.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-108.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-107.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-106.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-105.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-104.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-103.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-102.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-101.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-100.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-099.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-098.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-097.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-096.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-095.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-094.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-093.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-092.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-091.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-090.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-089.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-088.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-087.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-086.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-085.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-084.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-083.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-082.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-081.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-080.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-079.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-078.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-077.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-076.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-075.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-074.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-073.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-072.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-071.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-070.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-069.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-068.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-067.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-066.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-065.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-064.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-063.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-062.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-061.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-060.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-059.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-058.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-057.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-056.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-055.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-054.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-053.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-052.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-051.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-050.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-049.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-048.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-047.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-046.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-045.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-044.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-043.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-042.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-041.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-040.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-039.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-038.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-037.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-036.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-035.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-034.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-033.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-032.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-031.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-030.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-029.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-028.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-027.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-026.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-025.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-024.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-023.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-022.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-021.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-020.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-019.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-018.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-017.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-016.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-015.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-014.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-013.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-012.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-011.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-010.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-009.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-008.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-007.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-006.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-005.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-004.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-003.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-002.jpg",
+    "https://www.eunicedhivya.com/james-webb-telescope/assets/imgs/image-seq/ramped/ezgif-frame-001.jpg",
 ];
 
 
@@ -323,64 +437,78 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var reverseImg = images.reverse()
 
-    tweenImg.to(obj, 0.5,
+    tweenImg.to(obj, 0.2,
         {
-            curImg: reverseImg.length - 1,	// animate propery curImg to number of images
+            curImg: images.length - 1,	// animate propery curImg to number of images
             roundProps: "curImg",				// only integers so it can be used as an array index
             repeat: 0,									// repeat 3 times
             immediateRender: true,			// load first image automatically
             ease: Linear.easeNone,			// show every image the same ammount of time
-            onUpdate: function () {
-                $("#myimg").attr("src", reverseImg[obj.curImg]); // set the image source
+            onUpdate: function (e) {
+                $("#myimg").attr("src", images[obj.curImg]); // set the image source
             }
         }
-    );
-    // var tweenImg = TweenMax.to(obj, 0.5,
-    //     {
-    //         curImg: images.length - 1,	// animate propery curImg to number of images
-    //         roundProps: "curImg",				// only integers so it can be used as an array index
-    //         repeat: 3,									// repeat 3 times
-    //         immediateRender: true,			// load first image automatically
-    //         ease: Linear.easeNone,			// show every image the same ammount of time
-    //         onUpdate: function () {
-    //             $("#myimg").attr("src", images[obj.curImg]); // set the image source
-    //         }
-    //     }
-    // );
+    )
 
-    var scene5 = new ScrollMagic.Scene({ triggerElement: "#telescope", duration: 5000, triggerHook: 0 })
+
+    var scene5 = new ScrollMagic.Scene({ triggerElement: "#telescope", duration: 50000, triggerHook: 0 })
         .setTween(tweenImg)
+        .on("update", function (e) {
+            // console.log("start")
+            // console.log(controller.info("scrollDirection"));
+            console.log(e.scrollPos);
+            // endPos: 57874.84814453125
+            // namespace: ""
+            // scrollPos: 9133.3330078125
+            // startPos: 7874.84814453125
+            // target: _.Scene { on: ƒ, off: ƒ, trigger: ƒ, addTo: ƒ, enabled: ƒ, … }
+            // timeStamp: 1640106813111
+            // timestamp: 1640106813111
+            // type: "update"
+            if (controller.info("scrollDirection") === "FORWARD") {
+                if (e.scrollPos > 7874 && e.scrollPos < 10000) {
+                    $("#step1").css("opacity", 1)
+                } else if (e.scrollPos > 10500 && e.scrollPos < 15000) {
+                    $("#step1").css("opacity", 0)
+                    $("#step2").css("opacity", 1)
+                }
+            } else {
+                if (e.scrollPos > 7874 && e.scrollPos < 10000) {
+                    $("#step1").css("opacity", 0)
+                } else if (e.scrollPos > 10500 && e.scrollPos < 15000) {
+                    $("#step2").css("opacity", 0)
+                }
+            }
+        })
         .setPin("#telescope")
         .addIndicators() // add indicators (requires plugin)
         .addTo(controller);
 
-    console.log(scene5)
 
 
-    // // Grab the target video duration
-    // videoDuration = video.duration;
-    // console.log(video.duration * 1000);
 
-    // // Tween the video from beginning to end
-    // var videoTween = TweenMax.to(video, 1, {
-    //     currentTime: videoDuration,
-    //     ease: Quad.easeOut,
-    //     overwrite: true,
-    //     pause: true
-    // });
+    var timelineRocket = new TimelineMax();
+    timelineRocket
+        .from('#rocket-body', 4, {
+            y: 100,
+            opacity: 0,
+            ease: Power0.easeOut
+        })
+        .from('#fairing-left', 4, {
+            x: 100,
+            opacity: 0,
+            ease: Power0.easeOut
+        }, "one")
+        .from('#fairing-right', 4, {
+            x: -100,
+            opacity: 0,
+            ease: Power0.easeOut
+        }, "one")
 
-    // // Build ScrollMagic Scene
-    // var scene5 = new ScrollMagic.Scene({ triggerElement: "#telescope", duration: videoDuration * 1000, offset: 0 })
-    //     .setTween(videoTween) // Add tween to the scene
-    //     .addIndicators({ name: "video play" }) // Add indicators to the scrollbar							
-    //     .setPin("#telescope") // Add indicators to the scrollbar	
-    //     .setClassToggle("#telescope", "active")
-    //     .addTo(controller); // Add scene to the controller
-
-    // var scene6 = new ScrollMagic.Scene({
-    //     triggerElement: '#jwst-deployment',
-    //     duration: '100%',
-    //     triggerHook: 0,
-    // }).setTween().setPin("#jwst-deployment").setClassToggle("#jwst-deployment", "active").addTo(controller)
+    var scene6 = new ScrollMagic.Scene({ triggerElement: "#ariane", duration: "300%", triggerHook: 0 })
+        .setTween(timelineRocket)
+        .setPin("#ariane")
+        .addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
 
 });
